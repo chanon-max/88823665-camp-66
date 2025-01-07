017034b24dbaed7cfe195910fa84bc330c0ae604
chanon <chanonsrisa@gmail.com>
2025-01-07 19:48:55 +0700

    laravel01

--- mylaravel/routes/web.php
+++ mylaravel/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MyController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -8,3 +9,13 @@ Route::get('/', function () {
 Route::get('/hello', function () {
     return "<h1>HELLO WOLRD</h1>";
 });
+
+Route::get(
+    '/mycontroller/{id?}',
+    [MyController::class, 'myfunction']
+);
+
+Route::post(
+    '/mycontroller/{id?}',
+    [MyController::class, 'myfunction']
+);
